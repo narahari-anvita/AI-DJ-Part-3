3ee2e34a526844607601c8c3f886540baeed2470
narahari-anvita <79611794+narahari-anvita@users.noreply.github.com>
2022-01-05 15:35:42 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+
+song=""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function modelLoaded(){
+    console.log("Posenet Intialized");
+}
+
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristY = " + leftWristY + "leftWristX = " + leftWristX);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristY = " + rightWristY + "rightWristX = " + rightWristX);
+    }
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1.5);
+}
+
+function pause(){
+    song.pause();
+}
+
+function stop(){
+    song.stop();
+}
